7079563083b37aac5077b3e32aaa221a322b94f1
xiefei001 <xiefei001@hotmail.com>
2015-09-03 08:27:12 +0200

    merge test

--- src/tarifberater/tarifberater.js
+++ src/tarifberater/tarifberater.js
@@ -1,7 +1,5 @@
-/**
- * Created by xie on 01.09.2015.
- */
 'use strict';
+
 
 angular.module('tarifBerater.inputview', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
@@ -10,6 +8,13 @@ angular.module('tarifBerater.inputview', ['ngRoute'])
             controller: 'inputviewcontroller'
         });
     }])
+
+    // Purpose of controller: Binding data to view.
+    // Controller shouldn't hold model state.
+    // Controller accept input and converts it to command for model or view.
     .controller('inputviewcontroller', function () {
 
     });
+
+
+// Models There are lots of dirrerent ways to create a "model", aka services, factories and providers.
